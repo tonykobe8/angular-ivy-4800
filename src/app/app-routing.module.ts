3f5543ecd5f0d2278f19ae228d44930138f26fe0
tonykobe8 <tonykobe8@gmail.com>
2020-10-25 19:09:08 +0200

    2 files modified, 3 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { RegisterComponent } from './register/register.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { LoginComponent } from './login/login.component'
-import { HomeComponent } from './home/home.component';
+
 
 
 const appRoutes:Routes = [
-  {path:'' , component:HomeComponent},
+  
   {path:'login' ,component:LoginComponent },
   { path:'register',component:RegisterComponent},
   {path:'products-list' ,component:ProductsListComponent},
